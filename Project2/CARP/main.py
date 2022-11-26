fbfe78e93a94135e8a40f40ee5f2ce708c872d92
Faccococo <12012710@mail.sustech.edu.cn>
2022-11-26 21:20:10 +0800

    A commit

--- Project2/CARP/main.py
+++ Project2/CARP/main.py
@@ -10,9 +10,10 @@ RANDOM_SEED = args[4]
 
 file_args, graph, demand_edge = read_file(instance_filename)
 # graph = list(graph)
+print(graph)
 
 vertices_num = int(file_args['VERTICES'])
-depot_pos = int(file_args['DEPOT'])
+depot_pos = int(file_args['DEPOT']) - 1
 required_edge_num = int(file_args['REQUIRED EDGES'])
 non_required_edge_num = int(file_args['NON-REQUIRED EDGES'])
 edge_num = required_edge_num + non_required_edge_num
@@ -33,9 +34,13 @@ for route in routes:
     node = route[0]
     point_a = node[0][0]
     point_b = node[0][1]
+    cost_pre = cost
     cost += graph[depot_pos, point_a]
     cost += graph[point_a, point_b]
     cost += graph[point_b, depot_pos]
+    # print(depot_pos, "  ", end="")
+    print(point_a, point_b, " ", end="")
+    print(cost - cost_pre)
 
 for route in routes:
     result += "0,"
@@ -47,4 +52,9 @@ result = result.strip(',')
 result += "\n"
 result += "q "
 result += str(cost)
+
 print(result)
+
+# f = open("output.txt", "w")
+# f.write(result)
+# f.close()
