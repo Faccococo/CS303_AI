2d7189a1fdac69ef28b8598fdf51bbfed2be5dd0
Faccococo <12012710@mail.sustech.edu.cn>
2022-12-02 00:04:33 +0800

    A commit

--- Project2/CARP/Genetic.py
+++ Project2/CARP/Genetic.py
@@ -4,43 +4,31 @@ from utils import *
 def Genetic(depot, graph, distance, demand_graph, demand_edges, capacity, iter_num, random_seed, start, time,
             terminate):
     # init population
-    population = []
-    init_pop_num = 100
     mutate_rate = 0.2
-    for _ in range(init_pop_num):
-        routes, _ = path_scanning(depot, graph, distance, demand_graph, demand_edges, capacity, 100,
+
+    population, _ = path_scanning(depot, graph, distance, demand_graph, demand_edges, capacity, 100,
                                   random_seed,
                                   start, time,
                                   terminate)
-        population.append(routes)
 
     # 演化
     for gen in iter_num:
-        next_gen = Genetic_reduce(population, mutate_rate)
-        population = Genetic_replacement(population, next_gen)
+        next_gen = Genetic_reproduce()
+        population = Genetic_replacement()
 
-    final_routes = min(population, key=Genetic_fitness)
-    cost_out = cal_cost(final_routes, graph, distance, depot)
-    return final_routes, cost_out
+    cost_out = cal_cost(population, graph, distance, depot)
+    return population, cost_out
 
 
-def Genetic_reproduce(population, mutate_rate):
+def Genetic_reproduce():
     # TODO
-    return_list = []
-    for i in range(len(population)):
-        return_list.append(
-            Genetic_reproduce_mutate(
-                Genetic_reproduce_recombine(
-                    *Genetic_reproduce_select(population)
-                ),
-                mutate_rate
-            )
-        )
-    return return_list
+
+    return []
 
 
 def Genetic_reproduce_select(population):
     # TODO
+
     return []
 
 
@@ -54,12 +42,9 @@ def Genetic_reproduce_mutate(pop, mutate_rate):
     pass
 
 
-def Genetic_replacement(old, new):
-    # TODO
-    entity = old + new
-
+def Genetic_replacement():
     return []
 
 
-def Genetic_fitness(routes, graph, distance, depot):
-    return cal_cost(routes, graph, distance, depot)
+def Genetic_fitness():
+    return 0
